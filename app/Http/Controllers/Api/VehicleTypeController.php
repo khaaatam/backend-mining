8f681bcbf0e63c03079586204f6a4f961573e76d
khaaatam <gilbrankha1132@gmail.com>
2026-04-07 12:29:12 +0700

    verifikasi logika expiry status sukses pada testing api

--- app/Http/Controllers/Api/VehicleTypeController.php
+++ app/Http/Controllers/Api/VehicleTypeController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\VehicleType;
-use Illuminate\Http\Request;
 
 class VehicleTypeController extends Controller
 {
